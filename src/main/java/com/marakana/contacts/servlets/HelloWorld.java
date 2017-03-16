991b5a8ce6d6a0ee5cbb269fe5470e521dad4ed6
krishna <krishna@krishna-PC>
2017-03-16 18:25:21 +0530

    jsp redirect from servlet.

--- src/main/java/com/marakana/contacts/servlets/HelloWorld.java
+++ src/main/java/com/marakana/contacts/servlets/HelloWorld.java
@@ -2,13 +2,14 @@ package com.marakana.contacts.servlets;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+@WebServlet("/hello-jsp")
 public class HelloWorld extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -18,8 +19,9 @@ public class HelloWorld extends HttpServlet {
 			throws ServletException, IOException {
 
 		response.setContentType("text/html");
-		response.getWriter().println("<html><body><h1>HelloWorld</h1></body></html>");
-
+	//	response.getWriter().println("<html><body><h1>HelloWorld</h1></body></html>");
+		RequestDispatcher view = request.getRequestDispatcher("jsp/hello.jsp");
+		view.forward(request, response);
 	}
 
 }
